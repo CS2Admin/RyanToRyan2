8f59f0e7abffff266bf74ee47b6384d58c95cea0
Unknown <ryan.p.osterberg@gmail.com>
2018-02-16 08:38:46 -0600

    lots gone

--- src/Hello.java
+++ src/Hello.java
@@ -5,13 +5,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 System.out.println("Hello");
 System.out.println("Ryan");
-System.out.println("First");
-System.out.println("Second");
-System.out.println("Third");
-System.out.println("Third");
-System.out.println("Fourth");
-System.out.println("Fifth");
-System.out.println("Sixth");
+
 	}
 
 }
